2c8bc8eece8c53eed9a36ccc337bc1c390449cfd
Sporandum <sporandum@gmail.com>
2020-03-28 22:57:44 +0100

    Images Optimization

--- src/js/main-scripts.js
+++ src/js/main-scripts.js
@@ -1,8 +1,7 @@
+import 'lazysizes'
 import MenuDesktop from './modules/MenuDesktop'
 import MenuMobile from './modules/MenuMobile'
-
 import ToggleClassNameAfterHeader from './modules/ToggleClassNameAfterHeader'
-
 import LinkContact from './modules/LinkContact'
 
 new MenuDesktop(1599)
